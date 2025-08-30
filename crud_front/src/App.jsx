d31916b4c1b97af058831c2ee339c8fd368371ff
BORRA SATYAVATHI <satyavathiborra16@gmail.com>
2025-08-30 08:26:04 +0530

    Update App.jsx

--- crud_front/src/App.jsx
+++ crud_front/src/App.jsx
@@ -16,7 +16,7 @@ function App() {
   const handleChange = (e) => {
     setProduct({ ...product, [e.target.name]: e.target.value });
   };
-const BASE_URL = 'http://localhost:8080/springapp1';
+const BASE_URL = 'http://51.21.158.152:8080/springapp1';
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
